1e09ec92fe5a2b0e8d0774061f29c1a8f86db5bd
koarz <66543806+koarz@users.noreply.github.com>
2024-02-16 15:16:00 +0800

    Update forward_list.hpp

--- src/include/forward_list.hpp
+++ src/include/forward_list.hpp
@@ -1,10 +1,5 @@
 #pragma once
 
-#include "list.hpp"
-#include <cstddef>
-#include <forward_list>
-#include <iostream>
-#include <memory>
 namespace rbc {
 template <typename T> class forward_list;
 } // namespace rbc
@@ -15,7 +10,6 @@ public:
   using size_type = std::size_t;
   using reference = value_type &;
   using const_reference = const value_type &;
-  using allocator_type = std::allocator<T>;
 
   forward_list<T>() = default;
   ~forward_list<T>() = default;
@@ -33,11 +27,13 @@ public:
   void swap(forward_list &other);
   template <typename U> struct iterator {
     // 这几个是必须的，剩下自由发挥
-    iterator<U> operator++();
-    iterator<U> operator--();
-    reference operator*();
+    iterator<U> operator++() { return *this; }
+    reference operator*() {
+      T a;
+      return a;
+    }
     friend bool operator!=(const iterator<U> &lhs,
-                           const iterator<U> &rhs) noexcept;
+                           const iterator<U> &rhs) { return true; }
   };
   void insert_after(iterator<T> pos, const_reference value);
   void erase_after(iterator<T> first, iterator<T> last);
@@ -63,11 +59,11 @@ RBC_FORWARD_LIST_REF_V::front() {}
 
 RBC_FORWARD_LIST_C_REF_V::front() const {}
 
-template <typename T> bool rbc::forward_list<T>::empty() const {}
+template <typename T> bool rbc::forward_list<T>::empty() const { return true; }
 
 template <typename T>
 typename rbc::forward_list<T>::size_type
-rbc::forward_list<T>::max_size() const {}
+rbc::forward_list<T>::max_size() const { return 0; }
 
 RBC_FORWARD_LIST_VOID::push_front(rbc::forward_list<T>::const_reference value) {
 }
@@ -95,9 +91,3 @@ typename rbc::forward_list<T>::template iterator<T>
 rbc::forward_list<T>::end() {
   return iterator<T>();
 }
-
-namespace rbc {
-template <typename T>
-bool operator==(const std::forward_list<T> &lhs,
-                const std::forward_list<T> &rhs) {}
-} // namespace rbc
