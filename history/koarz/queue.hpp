046e67192508a47582c3bd391a72404e766dd76e
koarz <3577087577@qq.com>
2024-02-16 15:06:04 +0800

    remove queue

--- history/koarz/queue.hpp
+++ history/koarz/queue.hpp
@@ -82,7 +82,8 @@ RBC_QUEUE &rbc::queue<T>::operator=(const rbc::queue<T> &other) {
     if (!this_front->next && other_front) {
       this_front->next = new basic_queue<T>();
     }
-    this_front = this_front->next;
+    if (this_front->next)
+      this_front = this_front->next;
   }
   if (size() > other.size()) {
     delete this_front->next;
