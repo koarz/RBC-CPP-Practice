92fdd087cc5eb6a70d19c3c612e7e7cf8ad08c33
Eurekaxxxy <129188517+XEurekaX@users.noreply.github.com>
2024-02-17 11:39:55 +0800

    Update forward_list.hpp

--- src/include/forward_list.hpp
+++ src/include/forward_list.hpp
@@ -3,10 +3,11 @@
 namespace rbc {
 template <typename T> class forward_list;
 template <typename T> struct forward_list_node {
-  T data_{0};
+  T data_{};
   forward_list_node<T> *next_node_{nullptr};
-  forward_list_node<T>(T val) { data_ = val; }
+
   forward_list_node<T>() = default;
+  forward_list_node<T>(T val) { data_ = val; }
   ~forward_list_node<T>() { delete next_node_; }
 };
 
@@ -14,8 +15,7 @@ template <typename T> struct forward_list_node {
 
 template <typename T> class rbc::forward_list {
   forward_list_node<T> head_;
-  forward_list_node<T> tail_;
-  unsigned int size_{0};
+  unsigned int max_size_{1000000000};
   static forward_list_node<T> for_no_initialized_node;
 
 public:
@@ -25,13 +25,13 @@ public:
   using const_reference = const value_type &;
 
   forward_list<T>() = default;
-  ~forward_list<T>();
+  ~forward_list<T>() = default;
   forward_list<T> &operator=(const rbc::forward_list<T> &other);
   forward_list<T> &operator=(rbc::forward_list<T> &&other) noexcept;
   const_reference front() const;
   reference front();
   [[nodiscard]] bool empty() const;
-  [[nodiscard]] size_type size() const;
+  [[nodiscard]] size_type max_size() const;
   void assign(size_type count, const T &value);
   void clear() noexcept;
   void push_front(const_reference value);
@@ -41,10 +41,6 @@ public:
   template <typename U> struct iterator {
     iterator<U> operator++() {
       node_ = node_->next_node_ ?: nullptr;
-      return *this;
-    }
-    iterator<U> operator--() {
-      node_ = node_->pre_node ?: nullptr;
       return *this;
     }
     reference operator*() { return node_->data_; }
@@ -86,40 +82,25 @@ rbc::forward_list<T>::operator=(const rbc::forward_list<T> &other) {
     this_head->data_ = other_head->data_;
     other_head = other_head->next_node_;
     if (!this_head->next_node_ && other_head) {
-      this_head->next_node_ = new forward_list_node<T>(other_head->data_);
+      this_head->next_node_ = new forward_list_node<T>();
     }
-    this_head = this_head->next_node_;
-    if (this_head->next_node_ && !other_head) {
-      auto it = this_head->next_node_;
-      while (it) {
-        auto itt = it;
-        it = it->next_node_;
-        delete itt;
-      }
-    }
+    if (this_head->next_node_)
+      this_head = this_head->next_node_;
   }
-  size_ = other.size_;
+  if (max_size() > other.max_size()) {
+    delete this_head->next_node_;
+    this_head->next_node_ = nullptr;
+  }
+  max_size_ = other.max_size();
   return *this;
 }
 RBC_FORWARD_LIST &
 rbc::forward_list<T>::operator=(rbc::forward_list<T> &&other) noexcept {
   delete head_.next_node_;
   head_.next_node_ = other.head_.next_node_;
-  tail_.next_node_ = other.tail_.next_node_;
+  max_size_ = other.max_size();
   other.head_.next_node_ = nullptr;
-  other.tail_.next_node_ = nullptr;
   return *this;
-}
-
-// template <typename T> rbc::forward_list<T>::forward_list() {}
-
-template <typename T> rbc::forward_list<T>::~forward_list() {
-  auto it = head_.next_node_;
-  while (it != nullptr) {
-    auto itt = it;
-    it = it->next_node_;
-    delete itt;
-  }
 }
 
 RBC_FORWARD_LIST_REF_V::front() { return head_.next_node_->data_; }
@@ -131,92 +112,91 @@ template <typename T> bool rbc::forward_list<T>::empty() const {
 }
 
 template <typename T>
-typename rbc::forward_list<T>::size_type rbc::forward_list<T>::size() const {
-  return size_;
+typename rbc::forward_list<T>::size_type
+rbc::forward_list<T>::max_size() const {
+  return max_size_;
 }
 
 RBC_FORWARD_LIST_VOID::push_front(rbc::forward_list<T>::const_reference value) {
-  auto it = head_.next_node_;
-  head_.next_node_ = new forward_list_node<T>(value);
-  head_.next_node_->next_node_ = it;
-  size_++;
+  if (!head_.next_node_) {
+    head_.next_node_ = new forward_list_node<T>(value);
+  } else {
+    auto node = head_.next_node_;
+    head_.next_node_ = new forward_list_node<T>(value);
+    head_.next_node_->next_node_ = node;
+  }
 }
 
 RBC_FORWARD_LIST_VOID::pop_front() {
-  if (!head_.next_node_)
+  if (!head_.next_node_) {
     return;
-  auto node = head_.next_node_;
-  if (--size_ == 0) {
-    tail_.next_node_ = nullptr;
   }
-  head_.next_node_ = node->next_node_;
-  node->next_node_ = nullptr;
-  delete node;
+  if (!head_.next_node_->next_node_) {
+    delete head_.next_node_;
+    head_.next_node_ = nullptr;
+  } else {
+    auto node = head_.next_node_;
+    head_.next_node_ = node->next_node_;
+    node->next_node_ = nullptr;
+    delete node;
+  }
 }
 
 RBC_FORWARD_LIST_VOID::swap(rbc::forward_list<T> &other) {
   auto temp = head_.next_node_;
   head_.next_node_ = other.head_.next_node_;
   other.head_.next_node_ = temp;
-  temp = tail_.next_node_;
-  tail_.next_node_ = other.tail_.next_node_;
-  other.tail_.next_node_ = temp;
-  auto t_size = size_;
-  size_ = other.size_;
-  other.size_ = t_size;
+  auto t_size = max_size_;
+  max_size_ = other.max_size();
+  other.max_size_ = t_size;
 }
 
 RBC_FORWARD_LIST_VOID::resize(size_type count, const value_type &value) {
-  if (count == size_) {
-    return;
-  } else if (count > size_) {
-    size_ = count;
-    for (unsigned int i = 0; i < count - size_; i++) {
-      push_front(value);
+  size_type cnt = 0;
+  auto node = head_.next_node_;
+  while (node) {
+    cnt++;
+    if (node->next_node_) {
+      node = node->next_node_;
     }
-  } else {
-    size_ = count;
-    for (unsigned int i = 0; i < size_ - count; i++) {
-      pop_front();
+    if (cnt > count) {
+      node = nullptr;
+    }
+  }
+  if (cnt < count) {
+    auto x = count - cnt;
+    for (unsigned int i = 0; i < x; i++) {
+      push_front(value);
     }
   }
 }
 
 RBC_FORWARD_LIST_VOID::insert_after(iterator<T> pos, const_reference value) {
-  if (pos == begin()) {
-    push_front(value);
-  } else {
-    auto new_node = new forward_list_node<T>;
-    auto it = pos.next_node_;
-    pos.next_node_ = new_node;
-    new_node.next_node_ = it;
-    new_node->data_ = value;
-    size_++;
-  }
+  auto node = pos.node_->next_node_;
+  pos.node_->next_node_ = new forward_list_node<T>(value);
+  pos.node_->next_node_->next_node_ = node;
 }
 
 RBC_FORWARD_LIST_VOID::erase_after(iterator<T> first, iterator<T> last) {
-  for (auto i = first; i != last; i++) {
-    size_--;
-    delete i;
+  auto node = first.node_->next_node_;
+  while (node != last.node_) {
+    auto it = node;
+    node = node->next_node_;
+    delete it;
   }
-  first.next_node_ = last;
+  first.node_->next_node_ = last.node_;
 }
 
 RBC_FORWARD_LIST_VOID::clear() noexcept {
-  size_ = 0;
-  auto it = head_.next_node_;
-  while (it != nullptr) {
-    auto itt = it;
-    it = it->next_node_;
-    delete itt;
+  while (!empty()) {
+    pop_front();
   }
 }
 
 template <typename T>
 typename rbc::forward_list<T>::template iterator<T>
 rbc::forward_list<T>::begin() {
-  return iterator<T>(head_.next_node_);
+  return iterator<T>();
 }
 
 template <typename T>
