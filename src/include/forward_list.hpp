881bf8de7128f534d1c87bb8d6e11535133fb855
Eurekaxxxy <129188517+XEurekaX@users.noreply.github.com>
2024-02-17 11:59:47 +0800

    Update forward_list.hpp

--- src/include/forward_list.hpp
+++ src/include/forward_list.hpp
@@ -2,22 +2,9 @@
 
 namespace rbc {
 template <typename T> class forward_list;
-template <typename T> struct forward_list_node {
-  T data_{};
-  forward_list_node<T> *next_node_{nullptr};
-
-  forward_list_node<T>() = default;
-  forward_list_node<T>(T val) { data_ = val; }
-  ~forward_list_node<T>() { delete next_node_; }
-};
-
 } // namespace rbc
 
 template <typename T> class rbc::forward_list {
-  forward_list_node<T> head_;
-  unsigned int max_size_{1000000000};
-  static forward_list_node<T> for_no_initialized_node;
-
 public:
   using value_type = T;
   using size_type = unsigned int;
@@ -39,22 +26,15 @@ public:
   void resize(size_type count, const value_type &value);
   void swap(forward_list &other);
   template <typename U> struct iterator {
-    iterator<U> operator++() {
-      node_ = node_->next_node_ ?: nullptr;
-      return *this;
+    // 这几个是必须的，剩下自由发挥
+    iterator<U> operator++() { return *this; }
+    reference operator*() {
+      T a;
+      return a;
     }
-    reference operator*() { return node_->data_; }
-    template <typename V>
-    friend bool operator==(const iterator<V> &lhs, const iterator<V> &rhs) {
-      return lhs.node_ == rhs.node_;
+    friend bool operator!=(const iterator<U> &lhs, const iterator<U> &rhs) {
+      return true;
     }
-    template <typename V>
-    friend bool operator!=(const iterator<V> &lhs, const iterator<V> &rhs) {
-      return lhs.node_ != rhs.node_;
-    }
-    iterator<U>() = default;
-    iterator<U>(forward_list_node<U> *node) : node_(node) {}
-    forward_list_node<U> *node_{nullptr};
   };
   void insert_after(iterator<T> pos, const_reference value);
   void erase_after(iterator<T> first, iterator<T> last);
@@ -72,126 +52,36 @@ public:
 #define RBC_FORWARD_LIST_VOID template <typename T> void rbc::forward_list<T>
 
 RBC_FORWARD_LIST &
-rbc::forward_list<T>::operator=(const rbc::forward_list<T> &other) {
-  auto other_head = other.head_.next_node_;
-  auto this_head = head_.next_node_;
-  while (other_head) {
-    if (!head_.next_node_) {
-      head_.next_node_ = this_head = new forward_list_node<T>();
-    }
-    this_head->data_ = other_head->data_;
-    other_head = other_head->next_node_;
-    if (!this_head->next_node_ && other_head) {
-      this_head->next_node_ = new forward_list_node<T>();
-    }
-    if (this_head->next_node_)
-      this_head = this_head->next_node_;
-  }
-  if (max_size() > other.max_size()) {
-    delete this_head->next_node_;
-    this_head->next_node_ = nullptr;
-  }
-  max_size_ = other.max_size();
-  return *this;
-}
+rbc::forward_list<T>::operator=(const rbc::forward_list<T> &other) {}
 RBC_FORWARD_LIST &
-rbc::forward_list<T>::operator=(rbc::forward_list<T> &&other) noexcept {
-  delete head_.next_node_;
-  head_.next_node_ = other.head_.next_node_;
-  max_size_ = other.max_size();
-  other.head_.next_node_ = nullptr;
-  return *this;
-}
+rbc::forward_list<T>::operator=(rbc::forward_list<T> &&other) noexcept {}
 
-RBC_FORWARD_LIST_REF_V::front() { return head_.next_node_->data_; }
+RBC_FORWARD_LIST_REF_V::front() {}
 
-RBC_FORWARD_LIST_C_REF_V::front() const { return head_.next_node_->data_; }
+RBC_FORWARD_LIST_C_REF_V::front() const {}
 
-template <typename T> bool rbc::forward_list<T>::empty() const {
-  return head_.next_node_ == nullptr;
-}
+template <typename T> bool rbc::forward_list<T>::empty() const { return true; }
 
 template <typename T>
 typename rbc::forward_list<T>::size_type
 rbc::forward_list<T>::max_size() const {
-  return max_size_;
+  return 0;
 }
 
 RBC_FORWARD_LIST_VOID::push_front(rbc::forward_list<T>::const_reference value) {
-  if (!head_.next_node_) {
-    head_.next_node_ = new forward_list_node<T>(value);
-  } else {
-    auto node = head_.next_node_;
-    head_.next_node_ = new forward_list_node<T>(value);
-    head_.next_node_->next_node_ = node;
-  }
 }
 
-RBC_FORWARD_LIST_VOID::pop_front() {
-  if (!head_.next_node_) {
-    return;
-  }
-  if (!head_.next_node_->next_node_) {
-    delete head_.next_node_;
-    head_.next_node_ = nullptr;
-  } else {
-    auto node = head_.next_node_;
-    head_.next_node_ = node->next_node_;
-    node->next_node_ = nullptr;
-    delete node;
-  }
-}
+RBC_FORWARD_LIST_VOID::pop_front() {}
 
-RBC_FORWARD_LIST_VOID::swap(rbc::forward_list<T> &other) {
-  auto temp = head_.next_node_;
-  head_.next_node_ = other.head_.next_node_;
-  other.head_.next_node_ = temp;
-  auto t_size = max_size_;
-  max_size_ = other.max_size();
-  other.max_size_ = t_size;
-}
+RBC_FORWARD_LIST_VOID::swap(rbc::forward_list<T> &other) {}
 
-RBC_FORWARD_LIST_VOID::resize(size_type count, const value_type &value) {
-  size_type cnt = 0;
-  auto node = head_.next_node_;
-  while (node) {
-    cnt++;
-    if (node->next_node_) {
-      node = node->next_node_;
-    }
-    if (cnt > count) {
-      node = nullptr;
-    }
-  }
-  if (cnt < count) {
-    auto x = count - cnt;
-    for (unsigned int i = 0; i < x; i++) {
-      push_front(value);
-    }
-  }
-}
+RBC_FORWARD_LIST_VOID::resize(size_type count, const value_type &value) {}
 
-RBC_FORWARD_LIST_VOID::insert_after(iterator<T> pos, const_reference value) {
-  auto node = pos.node_->next_node_;
-  pos.node_->next_node_ = new forward_list_node<T>(value);
-  pos.node_->next_node_->next_node_ = node;
-}
+RBC_FORWARD_LIST_VOID::insert_after(iterator<T> pos, const_reference value) {}
 
-RBC_FORWARD_LIST_VOID::erase_after(iterator<T> first, iterator<T> last) {
-  auto node = first.node_->next_node_;
-  while (node != last.node_) {
-    auto it = node;
-    node = node->next_node_;
-    delete it;
-  }
-  first.node_->next_node_ = last.node_;
-}
+RBC_FORWARD_LIST_VOID::erase_after(iterator<T> first, iterator<T> last) {}
 
-RBC_FORWARD_LIST_VOID::clear() noexcept {
-  while (!empty()) {
-    pop_front();
-  }
-}
+RBC_FORWARD_LIST_VOID::clear() noexcept {}
 
 template <typename T>
 typename rbc::forward_list<T>::template iterator<T>
